1d63ce20b50e8cd79fd51616b800027a0b5a4458
jack <jackjocross@gmail.com>
2018-12-04 19:18:22 -0500

    Move hook to page component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
-import Counter from '../components/counter'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Counter />
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Layout>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div>
+        <p>
+          <b>Count: {count}</b>
+        </p>
+        <button onClick={() => setCount(count + 1)}> +1</button>
+        <button onClick={() => setCount(count - 1)}> -1</button>
+      </div>
+      <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
